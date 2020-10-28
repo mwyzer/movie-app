2b0d9a48fdf48d89fd0c5929cf2f6cd90be82885
Muhammad Wyzer <wyzer@Wyzer-MacBook-Pro.local>
2020-10-28 15:08:02 +0700

    Add useEffect

--- components/sideMenu.js
+++ components/sideMenu.js
@@ -1,14 +1,20 @@
-const SideMenu = () => {
-    return (
-        <div>
-            <h1 className="my-4">Shop Name</h1>
-                <div className="list-group">
-                <a href="#" className="list-group-item">Category 1</a>
-                <a href="#" className="list-group-item">Category 2</a>
-                <a href="#" className="list-group-item">Category 3</a>
-                </div>
-        </div>
-    )
-}
+import { useState } from 'react'
 
-export default SideMenu
+    const SideMenu = (props) => {
+        return (
+            <div>
+                <button onClick={props.clickHandler}> CLICK ME!</button>
+                <h1 className="my-4">{props.appName}</h1>
+                <div className="list-group">
+                    <a href="#" className="list-group-item">Category 1</a>
+                    <a href="#" className="list-group-item">Category 2</a>
+                    <a href="#" className="list-group-item">Category 3</a>
+                </div>
+                <div>
+                    
+                </div>
+            </div>
+        )
+    }
+
+    export default SideMenu
